b276f2b0e6b2f56526405b745ca21359b4eef4fc
Murilo <muriloazarias97@gmail.com>
2022-06-14 22:53:26 -0300

    Entrega Semanal Kenzie - JS

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = "Murilo"
+let sobrenome = "Azarias de Araújo"
+let anoNascimento = 1997
+let cidade = "São José do Rio Pardo - SP"
+
+let templateInfoCompleta = `${nome} ${sobrenome}, ${2022-1997} anos, natural de ${cidade}`;
